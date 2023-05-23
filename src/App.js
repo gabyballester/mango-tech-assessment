a04d793f437c295beff9cb92ad56ca3b35d09c80
Gabriel Ballester <gaby.ballester@gmail.com>
2023-05-23 15:36:18 +0200

    Add running scripts, settings for webpack, babel, support js and css/scss/css modules

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './App.modules.scss';
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      App component
+    </div>
+  )
+}
+
+export default App
