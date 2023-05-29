0a2ebf36d330807feaf163428059272ec715e778
Gabriel Ballester <gaby.ballester@gmail.com>
2023-05-29 17:46:28 +0200

    Tests Navbar

--- src/components/Nabvar/components/MenuList/index.jsx
+++ src/components/Nabvar/components/MenuList/index.jsx
@@ -4,10 +4,12 @@ import { NavLink } from 'react-router-dom';
 
 const menuRoutes = [
 	{
+		testid: 'exercise1',
 		label: 'Exercise 1',
 		path: '/exercise1',
 	},
 	{
+		testid: 'exercise2',
 		label: 'Exercise 2',
 		path: '/exercise2',
 	},
@@ -16,8 +18,9 @@ const menuRoutes = [
 export const MenuList = () => {
 	return (
 		<ul className={styles.ul}>
-			{menuRoutes.map(({ path, label }, index) => (
+			{menuRoutes.map(({ path, label, testid }, index) => (
 				<NavLink
+					data-testid={testid}
 					className={({ isActive }) => (isActive ? styles.active : '')}
 					key={path + index}
 					to={path}
