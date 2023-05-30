20f50eee65132ab61c63b0d7fd7de106fb527eb3
Gabriel Ballester <gaby.ballester@gmail.com>
2023-05-30 12:19:55 +0200

    Tests Exercise2

--- src/pages/Exercise2/_test_/Exercise2.test.jsx
+++ src/pages/Exercise2/_test_/Exercise2.test.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from '../../../App';
 import { Exercise2 } from '..';
+import { Range } from '../../../components';
 
 describe('Exercise 2 ', () => {
 	let rootApp = {};
@@ -14,12 +15,29 @@ describe('Exercise 2 ', () => {
 				<Exercise2 />
 			</BrowserRouter>
 		);
+		const { container: range } = render(
+			<BrowserRouter>
+				<Range />
+			</BrowserRouter>
+		);
 		rootApp.container = appContainer;
 		rootApp.exercise2Container = exercise2;
+		rootApp.range = range;
 	});
 
 	it('Should render Exercise 2', () => {
 		const { exercise2Container } = rootApp;
 		expect(exercise2Container).toBeInTheDocument();
+	});
+	
+	it('Should render Range', () => {
+		const { range } = rootApp;
+		expect(range).toBeInTheDocument();
+	});
+
+	it('Should render title', () => {
+		const text = 'Exercise 2: Fixed Range';
+		const titleElement = screen.getAllByText(text)[0];
+		expect(titleElement).toBeInTheDocument();
 	});
 });
